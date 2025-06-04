83d2f0f142608aa2c5f5d2e77fc3d714c6e6e41f
Isa <dn0992@naver.com>
2025-06-04 22:12:16 +0900

    feat: SentimentSummaryView 생성

---
+++ app/Public Opinion for Stock/Public Opinion for Stock/Presentation/DetailInfoView/SentimentSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SentimentSummaryView.swift
+//  Public Opinion for Stock
+//
+//  Created by 우정아 on 6/4/25.
+//
+
+import SwiftUI
+
+struct SentimentSummaryView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    SentimentSummaryView()
+}
