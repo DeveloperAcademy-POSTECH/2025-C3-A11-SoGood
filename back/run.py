7984fe102515adde830f3d5627d00bb6834521d2
bongjooncha <cbj4133@naver.com>
2025-06-07 03:44:08 +0900

    fix: 크롤링일자 run_job에 넣을 수 있게 수정.

--- back/run.py
+++ back/run.py
@@ -3,19 +3,20 @@ import asyncio
 import schedule
 from scripts.daily import daily_task
 
-async def job():
-    await daily_task()
+async def job(n):
+    await daily_task(n)
 
-def run_job():
-    asyncio.run(job())
+def run_job(n):
+    asyncio.run(job(n))
 
-run_job()
+run_job(n = 7)
 
 # 매일 아침 6시에 실행하도록 스케줄 설정
-schedule.every().day.at("06:00").do(run_job)
+schedule.every().day.at("06:00").do(run_job, n = 1)
 
 def main():
     while True:
+        print(f"크롤링 시간: {time.strftime("%Y-%m-%d %H:%M:%S")}")
         schedule.run_pending()
         time.sleep(1)
 
